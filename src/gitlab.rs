90afbd2a37857f79dc4cc01cd731bd0768609f27
Nicolas Bigaouette <nbigaouette@gmail.com>
2016-11-19 09:06:05 -0500

    Create a GitLab struct

---
+++ src/gitlab.rs
@@ -0,0 +1,31 @@
+
+
+pub const API_VERSION: u16 = 3;
+
+
+#[derive(Debug)]
+pub struct GitLab {
+    scheme: String,
+    domain: String,
+    port: u16,
+    private_token: String,
+}
+
+
+impl GitLab {
+    pub fn new_http(domain: &str, private_token: &str) -> GitLab {
+        GitLab::new("http", &domain, 80, &private_token)
+    }
+    pub fn new_https(domain: &str, private_token: &str) -> GitLab {
+        GitLab::new("https", &domain, 443, &private_token)
+    }
+
+    pub fn new(scheme: &str, domain: &str, port: u16, private_token: &str) -> GitLab {
+        GitLab {
+            scheme: scheme.to_string(),
+            domain: domain.to_string(),
+            port:   port,
+            private_token: private_token.to_string(),
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,28 @@
+// Inspired by http://python-gitlab.readthedocs.io/en/stable/
+
+pub mod gitlab;
+
+pub use gitlab::GitLab;
+
+
+
+
+
 #[cfg(test)]
 mod tests {
+    use gitlab::GitLab;
+
     #[test]
     fn it_works() {
+
+        let gl = GitLab::new("http", "gitlab.com", 80, "XXXXXXXXXXXXX");
+        println!("gl: {:?}", gl);
+
+        let gl = GitLab::new_http("gitlab.com", "XXXXXXXXXXXXX");
+        println!("gl: {:?}", gl);
+
+        let gl = GitLab::new_https("gitlab.com", "XXXXXXXXXXXXX");
+        println!("gl: {:?}", gl);
+
     }
 }
