b320b8c02fb732ee12ee829262cab64aee9ea21e
Nicolas Bigaouette <nbigaouette@gmail.com>
2016-11-19 15:28:25 -0500

    Get GitLab token from environment variable to test valid connection

--- src/lib.rs
+++ src/lib.rs
@@ -13,29 +13,37 @@ pub use gitlab::GitLab;
 
 #[cfg(test)]
 mod tests {
+    use std::env;
     use gitlab::GitLab;
     use hyper;
 
     #[test]
     fn it_works() {
 
+        let token = match env::var("GITLAB_TOKEN") {
+            Ok(val) => val,
+            Err(_)  => panic!("Please set environment variable 'GITLAB_TOKEN'"),
+        };
+
         let gl = GitLab::new("http", "gitlab.com", 80, "XXXXXXXXXXXXX");
         println!("gl: {:?}", gl);
         assert_eq!(gl.attempt_connection().unwrap().status, hyper::status::StatusCode::Unauthorized);
 
-        let gl = GitLab::new_http("gitlab.com", "XXXXXXXXXXXXX");
+        let gl = GitLab::new("http", "gitlab.com", 80, &token);
         println!("gl: {:?}", gl);
-        assert_eq!(gl.attempt_connection().unwrap().status, hyper::status::StatusCode::Unauthorized);
+        assert_eq!(gl.attempt_connection().unwrap().status, hyper::Ok);
 
-        let gl = GitLab::new_https("gitlab.com", "XXXXXXXXXXXXX");
+        let gl = GitLab::new_http("gitlab.com", &token);
         println!("gl: {:?}", gl);
-        assert_eq!(gl.attempt_connection().unwrap().status, hyper::status::StatusCode::Unauthorized);
+        assert_eq!(gl.attempt_connection().unwrap().status, hyper::Ok);
 
-        println!("gl.build_url(): {:?}", gl.build_url("test"));
+        let gl = GitLab::new_https("gitlab.com", &token);
+        println!("gl: {:?}", gl);
+        assert_eq!(gl.attempt_connection().unwrap().status, hyper::Ok);
 
         // Example from GitLab: https://docs.gitlab.com/ce/api/#basic-usage
-        let expected_url = "https://gitlab.example.com:443/api/v3/projects?private_token=9koXpg98eAheJpvBs5tK";
-        let gl = GitLab::new_https("gitlab.example.com", "9koXpg98eAheJpvBs5tK");
+        let expected_url = "https://gitlab.example.com:443/api/v3/projects?private_token=XXXXXXXXXXXXX";
+        let gl = GitLab::new_https("gitlab.example.com", "XXXXXXXXXXXXX");
         assert_eq!(gl.build_url("projects"), expected_url);
     }
 }
