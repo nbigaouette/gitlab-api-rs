6191d37d3ed898874685292f76a34bbf41da8dfc
Nicolas Bigaouette <nbigaouette@gmail.com>
2016-11-21 15:40:42 -0500

    List API commands for groups

--- src/groups.rs
+++ src/groups.rs
@@ -1,4 +1,14 @@
 
 pub struct GroupsListing {
+// https://docs.gitlab.com/ce/api/groups.html
+// List groups:                 GET /groups
+// List owned groups:           GET /groups/owned
+// List a group's projects:     GET /groups/:id/projects
+// Details of a group:          GET /groups/:id
+// New group:                   POST /groups
+// Transfer project to group:   POST  /groups/:id/projects/:project_id
+// Update group:                PUT /groups/:id
+// Remove group:                DELETE /groups/:id
+// Search for group:            GET /groups?search=foobar
 
 }
