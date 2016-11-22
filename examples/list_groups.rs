fcee6aceb6fd28b512077fa1b9737f319889c6aa
Nicolas Bigaouette <nbigaouette@gmail.com>
2016-11-21 23:44:58 -0500

    List owned groups

--- examples/list_groups.rs
+++ examples/list_groups.rs
@@ -33,4 +33,7 @@ fn main() {
     // let groups = gl.groups(Default::default());
     let groups = gl.groups(gitlab::groups::Listing::new().skip_groups(vec![1,2,3]).clone());
     println!("groups: {:?}", groups);
+
+    let owned_groups = gl.owned_groups();
+    println!("owned_groups: {:?}", owned_groups);
 }
