7cbf9461706fc06488a757208c3010d7d79a1ecf
Nicolas Bigaouette <nbigaouette@gmail.com>
2016-11-22 23:03:32 -0500

    Add projects search

--- src/projects/search.rs
+++ src/projects/search.rs
@@ -17,3 +17,167 @@
 //! | `query` | string | yes | A string contained in the project name |
 //! | `order_by` | string | no | Return requests ordered by `id`, `name`, `created_at` or `last_activity_at` fields |
 //! | `sort` | string | no | Return requests sorted in `asc` or `desc` order |
+
+
+use BuildQuery;
+
+
+#[derive(Debug, Copy, Clone)]
+pub enum ListingOrderBy {
+    Id,
+    Name,
+    // Path,
+    CreatedAt,
+    // UpdatedAt,
+    LastActivityAt,
+}
+
+
+#[derive(Debug, Copy, Clone)]
+pub enum ListingSort {
+    Asc,
+    Desc,
+}
+
+
+#[derive(Default, Debug, Clone)]
+pub struct Listing {
+    /// A string contained in the project name.
+    query:  String,
+    /// Return requests ordered by. Default is `ListingOrderBy::CreatedAt`.
+    order_by:  Option<ListingOrderBy>,
+    /// Return requests sorted. Default is `ListingSort::Desc`.
+    sort: Option<ListingSort>,
+}
+
+
+impl Listing {
+    pub fn new(query: String) -> Listing {
+        Listing {
+            query: query,
+            ..Default::default()
+        }
+    }
+    pub fn order_by(&mut self, order_by: ListingOrderBy) -> &mut Listing {
+        self.order_by = Some(order_by);
+        self
+    }
+    fn sort(&mut self, sort: ListingSort) -> &mut Listing {
+        self.sort = Some(sort);
+        self
+    }
+}
+
+
+impl BuildQuery for Listing {
+    fn build_query(&self) -> String {
+
+        let mut query = format!("projects/search/{}", self.query);
+
+        let amp_char = "&";
+        let none_char = "";
+        let mut split_char = &none_char;
+
+        // Append a "?" only if at least one of the `Option` is `Some(_)` or if
+        // strings contain something.
+        query.push_str(match (&self.order_by,
+                              &self.sort) {
+            (&None, &None) => "",
+            _ => "?",
+        });
+
+        self.order_by.map(|order_by| {
+            query.push_str(split_char);
+            split_char = &amp_char;
+
+            query.push_str("order_by=");
+            query.push_str(match order_by {
+                ListingOrderBy::Id => "id",
+                ListingOrderBy::Name => "name",
+                // ListingOrderBy::Path => "path",
+                ListingOrderBy::CreatedAt => "created_at",
+                // ListingOrderBy::UpdatedAt => "updated_at",
+                ListingOrderBy::LastActivityAt => "last_activity_at",
+            });
+        });
+
+        self.sort.map(|sort| {
+            query.push_str(split_char);
+            split_char = &amp_char;
+
+            query.push_str("sort=");
+            query.push_str(match sort {
+                ListingSort::Asc => "asc",
+                ListingSort::Desc => "desc",
+            });
+        });
+
+        query
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use BuildQuery;
+
+    const TEST_SEARCH_QUERY: &'static str = "SearchPattern";
+
+
+    #[test]
+    fn build_query_default() {
+        let expected_string = format!("projects/search/{}", TEST_SEARCH_QUERY);
+        let listing = Listing::new(TEST_SEARCH_QUERY.to_string());
+        let query = listing.build_query();
+        assert_eq!(query, expected_string);
+    }
+
+
+    #[test]
+    fn build_query_order_by() {
+        let expected_string = format!("projects/search/{}?order_by=id", TEST_SEARCH_QUERY);
+        let query = Listing::new(TEST_SEARCH_QUERY.to_string()).order_by(ListingOrderBy::Id).build_query();
+        assert_eq!(query, expected_string);
+
+        let expected_string = format!("projects/search/{}?order_by=name", TEST_SEARCH_QUERY);
+        let query = Listing::new(TEST_SEARCH_QUERY.to_string()).order_by(ListingOrderBy::Name).build_query();
+        assert_eq!(query, expected_string);
+
+        // let expected_string = format!("projects/search/{}?order_by=path", TEST_SEARCH_QUERY);
+        // let query = Listing::new(TEST_SEARCH_QUERY.to_string()).order_by(ListingOrderBy::Path).build_query();
+        // assert_eq!(query, expected_string);
+
+        let expected_string = format!("projects/search/{}?order_by=created_at", TEST_SEARCH_QUERY);
+        let query = Listing::new(TEST_SEARCH_QUERY.to_string()).order_by(ListingOrderBy::CreatedAt).build_query();
+        assert_eq!(query, expected_string);
+
+        // let expected_string = format!("projects/search/{}?order_by=updated_at", TEST_SEARCH_QUERY);
+        // let query = Listing::new(TEST_SEARCH_QUERY.to_string()).order_by(ListingOrderBy::UpdatedAt).build_query();
+        // assert_eq!(query, expected_string);
+
+        let expected_string = format!("projects/search/{}?order_by=last_activity_at", TEST_SEARCH_QUERY);
+        let query = Listing::new(TEST_SEARCH_QUERY.to_string()).order_by(ListingOrderBy::LastActivityAt).build_query();
+        assert_eq!(query, expected_string);
+    }
+
+
+    #[test]
+    fn build_query_sort() {
+        let expected_string = format!("projects/search/{}?sort=asc", TEST_SEARCH_QUERY);
+        let query = Listing::new(TEST_SEARCH_QUERY.to_string()).sort(ListingSort::Asc).build_query();
+        assert_eq!(query, expected_string);
+
+        let expected_string = format!("projects/search/{}?sort=desc", TEST_SEARCH_QUERY);
+        let query = Listing::new(TEST_SEARCH_QUERY.to_string()).sort(ListingSort::Desc).build_query();
+        assert_eq!(query, expected_string);
+    }
+
+
+    #[test]
+    fn groups_build_query_multiple() {
+        let expected_string = format!("projects/search/{}?order_by=created_at&sort=desc", TEST_SEARCH_QUERY);
+        let query = Listing::new(TEST_SEARCH_QUERY.to_string()).order_by(ListingOrderBy::CreatedAt).sort(ListingSort::Desc).build_query();
+        assert_eq!(query, expected_string);
+    }
+}
