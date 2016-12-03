5992fdfe11dbe4ff1b9c2d87dffdee0641319a7a
Nicolas Bigaouette <nbigaouette@gmail.com>
2016-12-03 14:20:47 -0500

    Change API for projects/owned

--- src/projects/owned.rs
+++ src/projects/owned.rs
@@ -21,33 +21,33 @@
 //! | `search` | string | no | Return list of authorized projects matching the search criteria |
 
 
-use BuildQuery;
-
 use serde_json;
+use serde_urlencoded;
 
+use BuildQuery;
 use gitlab::GitLab;
 use Projects;
-use projects::OwnedProjectListerInternal;
+use projects::{OwnedProjectListerInternal, ListingOrderBy};
 
 
 
-impl GitLab {
-    pub fn projects_owned(&self, listing: Listing) -> Result<Projects, serde_json::Error> {
-        let query = listing.build_query();
-        self.get(&query)
-    }
-}
+// impl GitLab {
+//     pub fn projects_owned(&self, listing: Listing) -> Result<Projects, serde_json::Error> {
+//         let query = listing.build_query();
+//         self.get(&query)
+//     }
+// }
 
 
-#[derive(Debug, Copy, Clone)]
-pub enum ListingOrderBy {
-    Id,
-    Name,
-    Path,
-    CreatedAt,
-    UpdatedAt,
-    LastActivityAt,
-}
+// #[derive(Debug, Copy, Clone)]
+// pub enum ListingOrderBy {
+//     Id,
+//     Name,
+//     Path,
+//     CreatedAt,
+//     UpdatedAt,
+//     LastActivityAt,
+// }
 
 
 
@@ -57,130 +57,195 @@ pub struct ProjectsLister<'a> {
     internal: OwnedProjectListerInternal,
 }
 
+impl<'a> ProjectsLister<'a> {
+    pub fn new(gl: &'a ::GitLab) -> ProjectsLister {
+        ProjectsLister {
+            gl: gl,
+            internal: OwnedProjectListerInternal {
+                archived: None,
+                visibility: None,
+                order_by: None,
+                sort: None,
+                search: None,
+            },
+        }
+    }
 
-#[derive(Default, Debug, Clone)]
-pub struct Listing {
-    /// Limit by archived status
-    archived: Option<bool>,
-    /// Limit by visibility.
-    visibility: Option<::ListingVisibility>,
-    /// Return requests ordered by. Default is `ListingOrderBy::CreatedAt`.
-    order_by: Option<ListingOrderBy>,
-    /// Return requests sorted. Default is `::ListingSort::Desc`.
-    sort: Option<::ListingSort>,
-    /// Return list of authorized projects matching the search criteria.
-    search: String,
+    pub fn archived(&'a mut self, archived: bool) -> &'a mut ProjectsLister {
+        self.internal.archived = Some(archived);
+        self
+    }
+
+    pub fn visibility(&'a mut self, visibility: ::ListingVisibility) -> &'a mut ProjectsLister {
+        self.internal.visibility = Some(visibility);
+        self
+    }
+
+    pub fn order_by(&'a mut self, order_by: ListingOrderBy) -> &'a mut ProjectsLister {
+        self.internal.order_by = Some(order_by);
+        self
+    }
+
+    pub fn sort(&'a mut self, sort: ::ListingSort) -> &'a mut ProjectsLister {
+        self.internal.sort = Some(sort);
+        self
+    }
+
+    pub fn search(&'a mut self, search: String) -> &'a mut ProjectsLister {
+        self.internal.search = Some(search);
+        self
+    }
+
+    /// Commit the lister: Query GitLab and return a list of projects.
+    pub fn list(&self) -> Projects {
+        // let query = serde_urlencoded::to_string(&self);
+        let query = self.build_query();
+        debug!("query: {:?}", query);
+
+        let projects: Result<Projects, serde_json::Error> = self.gl.get(&query);
+
+        projects.unwrap()
+    }
 }
 
-
-#[allow(dead_code)]
-impl Listing {
-    pub fn new() -> Listing {
-        Default::default()
-    }
-    pub fn archived(&mut self, archived: bool) -> &mut Listing {
-        self.archived = Some(archived);
-        self
-    }
-    pub fn visibility(&mut self, visibility: ::ListingVisibility) -> &mut Listing {
-        self.visibility = Some(visibility);
-        self
-    }
-    pub fn order_by(&mut self, order_by: ListingOrderBy) -> &mut Listing {
-        self.order_by = Some(order_by);
-        self
-    }
-    fn sort(&mut self, sort: ::ListingSort) -> &mut Listing {
-        self.sort = Some(sort);
-        self
-    }
-    pub fn search(&mut self, search: String) -> &mut Listing {
-        self.search = search;
-        self
-    }
-}
-
-
-impl BuildQuery for Listing {
+impl<'a> BuildQuery for ProjectsLister<'a> {
     fn build_query(&self) -> String {
 
+        let encoded = serde_urlencoded::to_string(&self.internal).unwrap();
         let mut query = String::from("projects/owned");
-
-        let amp_char = "&";
-        let none_char = "";
-        let mut split_char = &none_char;
-
-        // Append a "?" only if at least one of the `Option` is `Some(_)` or if
-        // strings contain something.
-        query.push_str(match (&self.archived,
-                              &self.visibility,
-                              &self.order_by,
-                              &self.sort,
-                              self.search.is_empty()) {
-            (&None, &None, &None, &None, true) => "",
-            _ => "?",
-        });
-
-        self.archived.map(|archived| {
-            query.push_str(split_char);
-            split_char = &amp_char;
-
-            if archived {
-                query.push_str("archived=true")
-            } else {
-                query.push_str("archived=false")
-            }
-        });
-
-        self.visibility.map(|visibility| {
-            query.push_str(split_char);
-            split_char = &amp_char;
-
-            query.push_str("visibility=");
-            query.push_str(match visibility {
-                ::ListingVisibility::Public => "public",
-                ::ListingVisibility::Internal => "internal",
-                ::ListingVisibility::Private => "private",
-            });
-        });
-
-        self.order_by.map(|order_by| {
-            query.push_str(split_char);
-            split_char = &amp_char;
-
-            query.push_str("order_by=");
-            query.push_str(match order_by {
-                ListingOrderBy::Id => "id",
-                ListingOrderBy::Name => "name",
-                ListingOrderBy::Path => "path",
-                ListingOrderBy::CreatedAt => "created_at",
-                ListingOrderBy::UpdatedAt => "updated_at",
-                ListingOrderBy::LastActivityAt => "last_activity_at",
-            });
-        });
-
-        self.sort.map(|sort| {
-            query.push_str(split_char);
-            split_char = &amp_char;
-
-            query.push_str("sort=");
-            query.push_str(match sort {
-                ::ListingSort::Asc => "asc",
-                ::ListingSort::Desc => "desc",
-            });
-        });
-
-        if !self.search.is_empty() {
-            query.push_str(split_char);
-            // split_char = &amp_char;
-
-            query.push_str("search=");
-            query.push_str(&self.search);
+        if !encoded.is_empty() {
+            query.push_str("?");
+            query.push_str(&encoded);
         }
 
         query
     }
 }
+
+
+// #[derive(Default, Debug, Clone)]
+// pub struct Listing {
+//     /// Limit by archived status
+//     archived: Option<bool>,
+//     /// Limit by visibility.
+//     visibility: Option<::ListingVisibility>,
+//     /// Return requests ordered by. Default is `ListingOrderBy::CreatedAt`.
+//     order_by: Option<ListingOrderBy>,
+//     /// Return requests sorted. Default is `::ListingSort::Desc`.
+//     sort: Option<::ListingSort>,
+//     /// Return list of authorized projects matching the search criteria.
+//     search: String,
+// }
+
+
+// #[allow(dead_code)]
+// impl Listing {
+//     pub fn new() -> Listing {
+//         Default::default()
+//     }
+//     pub fn archived(&mut self, archived: bool) -> &mut Listing {
+//         self.archived = Some(archived);
+//         self
+//     }
+//     pub fn visibility(&mut self, visibility: ::ListingVisibility) -> &mut Listing {
+//         self.visibility = Some(visibility);
+//         self
+//     }
+//     pub fn order_by(&mut self, order_by: ListingOrderBy) -> &mut Listing {
+//         self.order_by = Some(order_by);
+//         self
+//     }
+//     fn sort(&mut self, sort: ::ListingSort) -> &mut Listing {
+//         self.sort = Some(sort);
+//         self
+//     }
+//     pub fn search(&mut self, search: String) -> &mut Listing {
+//         self.search = search;
+//         self
+//     }
+// }
+//
+//
+// impl BuildQuery for Listing {
+//     fn build_query(&self) -> String {
+//
+//         let mut query = String::from("projects/owned");
+//
+//         let amp_char = "&";
+//         let none_char = "";
+//         let mut split_char = &none_char;
+//
+//         // Append a "?" only if at least one of the `Option` is `Some(_)` or if
+//         // strings contain something.
+//         query.push_str(match (&self.archived,
+//                               &self.visibility,
+//                               &self.order_by,
+//                               &self.sort,
+//                               self.search.is_empty()) {
+//             (&None, &None, &None, &None, true) => "",
+//             _ => "?",
+//         });
+//
+//         self.archived.map(|archived| {
+//             query.push_str(split_char);
+//             split_char = &amp_char;
+//
+//             if archived {
+//                 query.push_str("archived=true")
+//             } else {
+//                 query.push_str("archived=false")
+//             }
+//         });
+//
+//         self.visibility.map(|visibility| {
+//             query.push_str(split_char);
+//             split_char = &amp_char;
+//
+//             query.push_str("visibility=");
+//             query.push_str(match visibility {
+//                 ::ListingVisibility::Public => "public",
+//                 ::ListingVisibility::Internal => "internal",
+//                 ::ListingVisibility::Private => "private",
+//             });
+//         });
+//
+//         self.order_by.map(|order_by| {
+//             query.push_str(split_char);
+//             split_char = &amp_char;
+//
+//             query.push_str("order_by=");
+//             query.push_str(match order_by {
+//                 ListingOrderBy::Id => "id",
+//                 ListingOrderBy::Name => "name",
+//                 ListingOrderBy::Path => "path",
+//                 ListingOrderBy::CreatedAt => "created_at",
+//                 ListingOrderBy::UpdatedAt => "updated_at",
+//                 ListingOrderBy::LastActivityAt => "last_activity_at",
+//             });
+//         });
+//
+//         self.sort.map(|sort| {
+//             query.push_str(split_char);
+//             split_char = &amp_char;
+//
+//             query.push_str("sort=");
+//             query.push_str(match sort {
+//                 ::ListingSort::Asc => "asc",
+//                 ::ListingSort::Desc => "desc",
+//             });
+//         });
+//
+//         if !self.search.is_empty() {
+//             query.push_str(split_char);
+//             // split_char = &amp_char;
+//
+//             query.push_str("search=");
+//             query.push_str(&self.search);
+//         }
+//
+//         query
+//     }
+// }
 
 
 #[cfg(test)]
@@ -189,100 +254,100 @@ mod tests {
     use BuildQuery;
 
 
-    #[test]
-    fn build_query_default() {
-        let expected_string = "projects/owned";
-        let listing: Listing = Default::default();
-        let query = listing.build_query();
-        assert_eq!(query, expected_string);
-
-        let expected_string = "projects/owned";
-        let listing = Listing::new();
-        let query = listing.build_query();
-        assert_eq!(query, expected_string);
-    }
-
-
-    #[test]
-    fn build_query_archived() {
-        let expected_string = "projects/owned?archived=true";
-        let query = Listing::new().archived(true).build_query();
-        assert_eq!(query, expected_string);
-
-        let expected_string = "projects/owned?archived=false";
-        let query = Listing::new().archived(false).build_query();
-        assert_eq!(query, expected_string);
-    }
-
-
-    #[test]
-    fn build_query_visibility() {
-        let expected_string = "projects/owned?visibility=public";
-        let query = Listing::new().visibility(::ListingVisibility::Public).build_query();
-        assert_eq!(query, expected_string);
-
-        let expected_string = "projects/owned?visibility=internal";
-        let query = Listing::new().visibility(::ListingVisibility::Internal).build_query();
-        assert_eq!(query, expected_string);
-
-        let expected_string = "projects/owned?visibility=private";
-        let query = Listing::new().visibility(::ListingVisibility::Private).build_query();
-        assert_eq!(query, expected_string);
-    }
-
-
-    #[test]
-    fn build_query_order_by() {
-        let expected_string = "projects/owned?order_by=id";
-        let query = Listing::new().order_by(ListingOrderBy::Id).build_query();
-        assert_eq!(query, expected_string);
-
-        let expected_string = "projects/owned?order_by=name";
-        let query = Listing::new().order_by(ListingOrderBy::Name).build_query();
-        assert_eq!(query, expected_string);
-
-        let expected_string = "projects/owned?order_by=path";
-        let query = Listing::new().order_by(ListingOrderBy::Path).build_query();
-        assert_eq!(query, expected_string);
-
-        let expected_string = "projects/owned?order_by=created_at";
-        let query = Listing::new().order_by(ListingOrderBy::CreatedAt).build_query();
-        assert_eq!(query, expected_string);
-
-        let expected_string = "projects/owned?order_by=updated_at";
-        let query = Listing::new().order_by(ListingOrderBy::UpdatedAt).build_query();
-        assert_eq!(query, expected_string);
-
-        let expected_string = "projects/owned?order_by=last_activity_at";
-        let query = Listing::new().order_by(ListingOrderBy::LastActivityAt).build_query();
-        assert_eq!(query, expected_string);
-    }
-
-
-    #[test]
-    fn build_query_sort() {
-        let expected_string = "projects/owned?sort=asc";
-        let query = Listing::new().sort(::ListingSort::Asc).build_query();
-        assert_eq!(query, expected_string);
-
-        let expected_string = "projects/owned?sort=desc";
-        let query = Listing::new().sort(::ListingSort::Desc).build_query();
-        assert_eq!(query, expected_string);
-    }
-
-
-    #[test]
-    fn build_query_search() {
-        let expected_string = "projects/owned?search=SearchPattern";
-        let query = Listing::new().search(String::from("SearchPattern")).build_query();
-        assert_eq!(query, expected_string);
-    }
-
-
-    #[test]
-    fn groups_build_query_multiple() {
-        let expected_string = "projects/owned?archived=false&sort=asc";
-        let query = Listing::new().archived(false).sort(::ListingSort::Asc).build_query();
-        assert_eq!(query, expected_string);
-    }
+    // #[test]
+    // fn build_query_default() {
+    //     let expected_string = "projects/owned";
+    //     let listing: Listing = Default::default();
+    //     let query = listing.build_query();
+    //     assert_eq!(query, expected_string);
+    //
+    //     let expected_string = "projects/owned";
+    //     let listing = Listing::new();
+    //     let query = listing.build_query();
+    //     assert_eq!(query, expected_string);
+    // }
+    //
+    //
+    // #[test]
+    // fn build_query_archived() {
+    //     let expected_string = "projects/owned?archived=true";
+    //     let query = Listing::new().archived(true).build_query();
+    //     assert_eq!(query, expected_string);
+    //
+    //     let expected_string = "projects/owned?archived=false";
+    //     let query = Listing::new().archived(false).build_query();
+    //     assert_eq!(query, expected_string);
+    // }
+    //
+    //
+    // #[test]
+    // fn build_query_visibility() {
+    //     let expected_string = "projects/owned?visibility=public";
+    //     let query = Listing::new().visibility(::ListingVisibility::Public).build_query();
+    //     assert_eq!(query, expected_string);
+    //
+    //     let expected_string = "projects/owned?visibility=internal";
+    //     let query = Listing::new().visibility(::ListingVisibility::Internal).build_query();
+    //     assert_eq!(query, expected_string);
+    //
+    //     let expected_string = "projects/owned?visibility=private";
+    //     let query = Listing::new().visibility(::ListingVisibility::Private).build_query();
+    //     assert_eq!(query, expected_string);
+    // }
+    //
+    //
+    // #[test]
+    // fn build_query_order_by() {
+    //     let expected_string = "projects/owned?order_by=id";
+    //     let query = Listing::new().order_by(ListingOrderBy::Id).build_query();
+    //     assert_eq!(query, expected_string);
+    //
+    //     let expected_string = "projects/owned?order_by=name";
+    //     let query = Listing::new().order_by(ListingOrderBy::Name).build_query();
+    //     assert_eq!(query, expected_string);
+    //
+    //     let expected_string = "projects/owned?order_by=path";
+    //     let query = Listing::new().order_by(ListingOrderBy::Path).build_query();
+    //     assert_eq!(query, expected_string);
+    //
+    //     let expected_string = "projects/owned?order_by=created_at";
+    //     let query = Listing::new().order_by(ListingOrderBy::CreatedAt).build_query();
+    //     assert_eq!(query, expected_string);
+    //
+    //     let expected_string = "projects/owned?order_by=updated_at";
+    //     let query = Listing::new().order_by(ListingOrderBy::UpdatedAt).build_query();
+    //     assert_eq!(query, expected_string);
+    //
+    //     let expected_string = "projects/owned?order_by=last_activity_at";
+    //     let query = Listing::new().order_by(ListingOrderBy::LastActivityAt).build_query();
+    //     assert_eq!(query, expected_string);
+    // }
+    //
+    //
+    // #[test]
+    // fn build_query_sort() {
+    //     let expected_string = "projects/owned?sort=asc";
+    //     let query = Listing::new().sort(::ListingSort::Asc).build_query();
+    //     assert_eq!(query, expected_string);
+    //
+    //     let expected_string = "projects/owned?sort=desc";
+    //     let query = Listing::new().sort(::ListingSort::Desc).build_query();
+    //     assert_eq!(query, expected_string);
+    // }
+    //
+    //
+    // #[test]
+    // fn build_query_search() {
+    //     let expected_string = "projects/owned?search=SearchPattern";
+    //     let query = Listing::new().search(String::from("SearchPattern")).build_query();
+    //     assert_eq!(query, expected_string);
+    // }
+    //
+    //
+    // #[test]
+    // fn groups_build_query_multiple() {
+    //     let expected_string = "projects/owned?archived=false&sort=asc";
+    //     let query = Listing::new().archived(false).sort(::ListingSort::Asc).build_query();
+    //     assert_eq!(query, expected_string);
+    // }
 }
