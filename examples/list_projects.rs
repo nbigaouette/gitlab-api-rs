b337b259d07732eaded9c12450242ba937d15923
Nicolas Bigaouette <nbigaouette@gmail.com>
2016-12-03 21:37:12 -0500

    Add example for `projects::all`

--- examples/list_projects.rs
+++ examples/list_projects.rs
@@ -56,6 +56,9 @@ fn main() {
     let projects = gl.projects().owned().archived(false).list();
     println!("projects: {:?}", projects);
 
+    let projects = gl.projects().all().order_by(gitlab::projects::ListingOrderBy::Name).list();
+    println!("projects: {:?}", projects);
+
 
 
 
