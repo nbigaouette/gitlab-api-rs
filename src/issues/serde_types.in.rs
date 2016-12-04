08bb8d990063af51bcbf11b0162b785dcc30012b
Nicolas Bigaouette <nbigaouette@gmail.com>
2016-12-04 14:36:40 -0500

    Add issues serializable

---
+++ src/issues/serde_types.in.rs
@@ -0,0 +1,30 @@
+
+
+#[derive(Debug, Copy, Clone, Serialize, Deserialize)]
+pub enum State {
+    #[serde(rename = "opened")]
+    Opened,
+    #[serde(rename = "closed")]
+    Closed,
+}
+
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+pub enum ListingOrderBy {
+    #[serde(rename = "created_at")]
+    CreatedAt,
+    #[serde(rename = "updated_at")]
+    UpdatedAt,
+}
+
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+struct IssuesListerInternal {
+    /// State of issues to return.
+    state: Option<State>,
+    /// Labels of issues to return.
+    labels: Option<Vec<String>>,
+    /// Return requests ordered by. Default is `ListingOrderBy::CreatedAt`.
+    order_by: Option<ListingOrderBy>,
+    /// Return requests sorted. Default is `::ListingSort::Desc`.
+    sort: Option<::ListingSort>,
+}
