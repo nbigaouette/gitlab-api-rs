cbb5ef84d83b98023cdfcc448b6047ba37b78926
Nicolas Bigaouette <nbigaouette@gmail.com>
2016-12-05 11:51:05 -0500

    Cleanup example

--- examples/list_projects.rs
+++ examples/list_projects.rs
@@ -33,20 +33,6 @@ fn main() {
         }
     };
 
-    // // Projects
-    // // List projects
-    // let projects = gl.projects().list()
-    // let projects = gl.projects().archived(...).visibility(...).order_by(...).sort(...).search(...).simple(...).list()
-    // // List all projects (admin)
-    // let projects = gl.projects().all().list()
-    // let projects = gl.projects().all().archived(...).visibility(...).order_by(...).sort(...).search(...).list()
-    // // Single project
-    // let project = gl.projects().id(142).list()
-    // // Search
-    // let projects = gl.projects().search(...).list()
-    // // Owned
-    // let projects = gl.projects().owned().archived(...).visibility(...).order_by(...).sort(...).search(...).list()
-
     let gl = GitLab::new(&hostname, &token);
 
     let projects = gl.projects().list();
@@ -61,28 +47,4 @@ fn main() {
     let projects = gl.projects().all().order_by(gitlab::projects::ListingOrderBy::Name).list();
     println!("projects: {:?}", projects);
 
-
-
-
-    // // for i in 1..82 {
-    // //     gl.set_pagination(Pagination{page: i, per_page: 1});
-    // //     println!("projects: {:?}", gl.projects_list().unwrap());
-    // // }
-    // // gl.set_pagination(Pagination {
-    // //     page: 1,
-    // //     per_page: 100,
-    // // });
-    // let projects = gl.projects_list(projects::Listing::new()).unwrap();
-    // println!("projects: {:?}", projects);
-    // // FIXME: Project's members are private
-    // // for project in projects {
-    // //     println!("{:?}", project.path_with_namespace);
-    // // }
-    //
-    // let projects = gl.projects_all(projects::all::Listing::new()).unwrap();
-    // println!("projects: {:?}", projects);
-    //
-    // let listing = projects::id::Listing::new(projects::id::ListingId::Id(10));
-    // let projects = gl.project_id(listing).unwrap();
-    // println!("projects: {:?}", projects);
 }
