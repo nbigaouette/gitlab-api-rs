f1542ce9d677b65ede117e8d13ca0de8c2fbbc2c
Nicolas Bigaouette <nbigaouette@gmail.com>
2016-12-08 17:28:12 -0500

    Add `issues::projects` unit test `build_query_milestone()`

--- src/issues/project.rs
+++ src/issues/project.rs
@@ -238,6 +238,17 @@ mod tests {
 
 
     #[test]
+    fn build_query_milestone() {
+        let gl = ::GitLab::new(&"localhost", "XXXXXXXXXXXXXXXXXXXX").unwrap();
+        // let gl: ::GitLab = Default::default();
+
+        let expected_string = format!("projects/{}/issues?milestone=Test+Milestone", TEST_PROJECT_ID);
+        let query = gl.issues().project(TEST_PROJECT_ID).milestone("Test Milestone".to_string()).build_query();
+        assert_eq!(query, expected_string);
+    }
+
+
+    #[test]
     fn build_query_state() {
         let gl = ::GitLab::new(&"localhost", "XXXXXXXXXXXXXXXXXXXX").unwrap();
         // let gl: ::GitLab = Default::default();
