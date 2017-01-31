bb218594bbfc3de9d14795d7f5dbb9724e507802
Nicolas Bigaouette <nbigaouette@gmail.com>
2017-01-31 13:27:47 -0500

    Add a test deserializing a search for project
    To get the json data:
        curl "https://gitlab.com/api/v3/projects/search/gitlab-api-rs?private_token=${GITLAB_TOKEN}&page=1&per_page=20"

--- src/lib.rs
+++ src/lib.rs
@@ -66,6 +66,8 @@ pub trait Lister<T> {
 mod tests {
     // use gitlab::GitLab;
     // use hyper;
+    use serde_json;
+    use std::error::Error;
 
     // #[test]
     // fn unauthorized() {
@@ -74,4 +76,63 @@ mod tests {
     //     assert_eq!(gl.attempt_connection().unwrap().status,
     //                hyper::status::StatusCode::Unauthorized);
     // }
+
+    #[test]
+    fn deserialize_project() {
+        let json_reply = r##"[
+            {
+                "id": 517564,
+                "description": "GitLab API library and client in Rust",
+                "default_branch": "master",
+                "tag_list": [],
+                "public": false,
+                "archived": false,
+                "visibility_level": 0,
+                "ssh_url_to_repo": "git@gitlab.com:nbigaouette1/gitlab-api-rs.git",
+                "http_url_to_repo": "https://gitlab.com/nbigaouette1/gitlab-api-rs.git",
+                "web_url": "https://gitlab.com/nbigaouette1/gitlab-api-rs",
+                "owner": {
+                    "name": "Nicolas Bigaouette",
+                    "username": "nbigaouette1",
+                    "id": 163821,
+                    "state": "active",
+                    "avatar_url": "https://secure.gravatar.com/avatar/3325e461df2fda8738f35a8bf4fd735e?s=80&d=identicon",
+                    "web_url": "https://gitlab.com/nbigaouette1"
+                },
+                "name": "gitlab-api-rs",
+                "name_with_namespace": "Nicolas Bigaouette / gitlab-api-rs",
+                "path": "gitlab-api-rs",
+                "path_with_namespace": "nbigaouette1/gitlab-api-rs",
+                "container_registry_enabled": null,
+                "issues_enabled": true,
+                "merge_requests_enabled": true,
+                "wiki_enabled": true,
+                "builds_enabled": false,
+                "snippets_enabled": false,
+                "created_at": "2015-10-09T00:32:18.646Z",
+                "last_activity_at": "2017-01-31T14:46:26.638Z",
+                "shared_runners_enabled": true,
+                "lfs_enabled": true,
+                "creator_id": 163821,
+                "namespace": {
+                    "id": 193119,
+                    "name": "nbigaouette1",
+                    "path": "nbigaouette1",
+                    "kind": "user"
+                },
+                "avatar_url": null,
+                "star_count": 0,
+                "forks_count": 0,
+                "open_issues_count": 1,
+                "public_builds": true,
+                "shared_with_groups": [],
+                "only_allow_merge_if_build_succeeds": false,
+                "request_access_enabled": true,
+                "only_allow_merge_if_all_discussions_are_resolved": null,
+                "approvals_before_merge": 0
+            }
+        ]"##;
+
+        let project: ::Projects = serde_json::from_str(json_reply).expect("JSON deserialization failed");
+    }
 }
